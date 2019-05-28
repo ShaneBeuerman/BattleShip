20e81cfeb58e7095ee6242e893725986eff2f265
ShaneBeuerman <shanebeuerman@gmail.com>
2019-05-28 14:39:08 -0600

    Cleaned up a lot of things
    Eliminated a lot of unnecessary lines and  made it easier to read with more comments and less useless functions.

--- src/battleship/main.java
+++ src/battleship/main.java
@@ -11,84 +11,86 @@ import javax.swing.JPanel;
 
 public class main {
 
+    /*
+     main function()
+     */
     public static void main(String[] args) {
-        char[][] board = new char[10][10];
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                board[i][j] = '-';
-            }
-        }
         setupGUI();
     }
-    
-    public static void enemySetShips(char[][] board){
+
+    /*
+     setShip takes in a 10x10 board and marks a ships position with a 'X'
+     value. It randomly chooses a position on the board and checks to see
+     if the ship can fit in the part of the board.
+     */
+    public static void setShip(char[][] board, int n) {
+        Random rand = new Random();
+        int x = rand.nextInt(10);
+        int y = rand.nextInt(10);
+        int direction = rand.nextInt(2);
+        if (direction == 0) {
+            if (x + n > 9) {
+                setShip(board, n);
+            } else if (checkAhead(board, x, y, direction, n)) {
+                for (int i = 0; i < n; i++) {
+                    board[x + i][y] = 'X';
+                }
+            } else {
+                setShip(board, n);
+            }
+        } else {
+            if (y + n > 9) {
+                setShip(board, n);
+            } else if (checkAhead(board, x, y, direction, n)) {
+                for (int i = 0; i < n; i++) {
+                    board[x][y + i] = 'X';
+                }
+            } else {
+                setShip(board, n);
+            }
+        }
+
+    }
+
+    /*
+     checkAhead checks if the board already is marked with a ship ahead of it.
+     If the ship cannot be inserted into the board without colliding with another
+     ship, then it returns false. If it finds no collision, it returns true.
+     */
+    public static boolean checkAhead(char[][] board, int x, int y, int d, int n) {
+        if (d == 0) {
+            for (int i = 0; i < n; i++) {
+                if (board[x + i][y] == 'X') {
+                    return false;
+                }
+            }
+        } else {
+            for (int i = 0; i < n; i++) {
+                if (board[x][y + i] == 'X') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    /*
+     SetupGUI() creates a 10x10 grid of buttons that you can click.
+     If you click a button, it will be marked with an 'X' if it is
+     a hit, and marked with a 'O' if it is a miss.
+     */
+    public static void setupGUI() {
+        char[][] board = new char[10][10];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                board[i][j] = 'O';
+            }
+        }
         setShip(board, 5);
         setShip(board, 4);
         setShip(board, 3);
         setShip(board, 3);
         setShip(board, 2);
-        
-    }
-    
-    public static void setShip(char[][] board, int n){
-        Random rand = new Random();
-        int x = rand.nextInt(10);
-        int y = rand.nextInt(10);
-        int direction = rand.nextInt(2);
-        int counter = 0;
-        if(direction == 0){
-            if(x + n > 9){
-                setShip(board, n);
-            }
-            else{
-                for(int i = 0; i < n; i++){
-                    if(board[x+i][y] == 'X'){
-                        setShip(board, n);
-                    }
-                    else{
-                        counter++;
-                    }
-                }
-                if(counter == n){
-                    for(int i = 0; i < n; i++){
-                        board[x+i][y] = 'X';
-                    }
-                }
-            }
-        } else {
-            if (y + n > 9) {
-                setShip(board, n);
-            } else {
-                for (int i = 0; i < n; i++) {
-                    if (board[x][y+i] == 'X') {
-                        setShip(board, n);
-                    } else {
-                        counter++;
-                    }
-                }
-                if (counter == n) {
-                    for (int i = 0; i < n; i++) {
-                        board[x][y+i] = 'X';
-                    }
-                }
-            }
-        }
-        
-    }
-    
-    
-    public static void mark(char[][] board, int x, int y){
-        board[x][y] = 'X';
-    }
-    
-    public static void setupGUI(){
-        char[][] board = new char[10][10];
-        for(int i = 0; i < 10; i++){
-            for(int j = 0; j < 10; j++){
-                board[i][j] = 'O';
-            }
-        }
-        enemySetShips(board);
         JFrame game = new JFrame("Battleship");
         game.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         JButton[][] buttons = new JButton[10][10];
@@ -104,7 +106,7 @@ public class main {
                 buttons[i][j].addActionListener(new ActionListener() {
                     @Override
                     public void actionPerformed(ActionEvent arg) {
-                        if (hit(board, currentI, currentJ)) {
+                        if (board[currentI][currentJ] == 'X') {
                             buttons[currentI][currentJ].setText("X");
                         } else {
                             buttons[currentI][currentJ].setText("O");
@@ -117,14 +119,5 @@ public class main {
         game.add(grid);
         game.pack();
         game.setVisible(true);
-
     }
-
-    public static boolean hit(char[][] board, int x, int y) {
-        if(board[x][y] == 'X'){
-            return true;
-        }
-        return false;
-    }
-
 }
